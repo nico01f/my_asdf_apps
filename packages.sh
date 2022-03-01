58d111d29fcef17ab105dd3aef6cb3c398035acd
Nico F <nico01f@gmail.com>
2022-03-02 00:36:19 +0100

    My asdf packages

---
+++ packages.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+asdf plugin-add python https://github.com/danhper/asdf-python.git
+asdf install python 3.9.9
+asdf plugin-add terraform https://github.com/asdf-community/asdf-hashicorp.git
+asdf install terraform 1.1.2
+asdf plugin-add helm https://github.com/Antiarchitect/asdf-helm.git
+asdf install helm 3.7.2
+asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
+asdf install ruby 3.0.2
+asdf plugin-add gcloud https://github.com/jthegedus/asdf-gcloud
+asdf install gcloud 373.0.0
+asdf plugin-add httpie-go https://github.com/abatilo/asdf-httpie-go.git
+asdf install httpie-go 0.7.0
+asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
+asdf install golang 1.17.7
+asdf plugin-add syncher https://github.com/nwillc/syncher.git
+asdf install syncher 0.0.45
